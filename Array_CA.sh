82abc0bce615ffbf17a2f515bb262ff6e6d1bf99
AniysahFauziyyahAlfa <113014431+AniysahFauziyyahAlfa@users.noreply.github.com>
2022-10-12 19:26:47 +0700

    Add files via upload

---
+++ Array_CA.sh
@@ -0,0 +1,8 @@
+#deklarasi array compud assignmet
+distroLinuxDekstop=('BlankOn' 'Ubuntu' 'Debian' 'ArchLinux' 'LinuxMint')
+distroLinuxServer=('UbuntuServer' 'CentOS' 'FedoraServer')
+
+#cara mengambil milai arrray
+echo ${distroLinuxDekstop[*]}
+echo ${distroLinuxServer[*]}
+
