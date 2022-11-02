6ae93af5db8dc5e1d000dcca29b4055b82a043bc
AniysahFauziyyahAlfa <113014431+AniysahFauziyyahAlfa@users.noreply.github.com>
2022-11-02 19:42:51 +0700

    Add files via upload

---
+++ Tugas_7.sh
@@ -0,0 +1,22 @@
+# mendeklarasikan fungsi
+panjang() {
+       echo "Masukkan Panjang :"
+       read p
+}
+
+lebar() {
+     echo "Masukkan Lebar :"
+     read l
+}
+
+luas() {
+     echo "Program Menghitung Luas Bidang Persegi"
+     panjang
+     lebar
+     let l=$p*$l
+     echo "Luas Persegi :
+$l"
+}
+
+#memanggil fungsi
+luas
